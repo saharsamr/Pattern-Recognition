d4e2b6c3d736185644e99709950a4b9c95ef23b4
Sahar Rajabi <sahar.rajabi76@gmail.com>
2019-05-08 00:26:42 +0430

    calcuate prior probabilities/

--- BayesGussianParametricEstimate/estimation_and_classification.py
+++ BayesGussianParametricEstimate/estimation_and_classification.py
@@ -9,12 +9,19 @@ def sigma_estimate_ml(data, mu):
     return (1 / len(data)) * (np.matmul(np.transpose(data - mu), (data - mu)))
 
 
+def calc_prior_probabilities(labels):
+    labels, counts = np.unique(labels, return_counts=True)
+    return counts / sum(counts)
+
+
 if __name__ == '__main__':
 
     train_data = np.genfromtxt('data/Reduced Fashion-MNIST/Train_Data.csv', delimiter=',')
-    train_label = np.genfromtxt('data/Reduced Fashion-MNIST/Train_Labels.csv', delimiter=',')
+    train_labels = np.genfromtxt('data/Reduced Fashion-MNIST/Train_Labels.csv', delimiter=',')
 
     mu = mu_estimate_ml(train_data)
     sigma = sigma_estimate_ml(train_data, mu)
+
+    priors = calc_prior_probabilities(train_labels)
 
 
