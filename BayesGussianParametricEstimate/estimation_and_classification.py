dfad4a9264126d4a43b7c63e7003d9fd93258e46
Sahar Rajabi <sahar.rajabi76@gmail.com>
2019-05-08 11:44:46 +0430

    adding baysian classifier, not working!

--- BayesGussianParametricEstimate/estimation_and_classification.py
+++ BayesGussianParametricEstimate/estimation_and_classification.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import multivariate_normal
 
 
 def mu_estimate_ml(data):
@@ -11,7 +12,22 @@ def sigma_estimate_ml(data, mu):
 
 def calc_prior_probabilities(labels):
     labels, counts = np.unique(labels, return_counts=True)
-    return counts / sum(counts)
+    return labels, counts / sum(counts)
+
+
+def classify_baysian(data, mu, sigma, priors, labels):
+    classes = np.zeros(len(data))
+    for i, sample in enumerate(data):
+        max_prob = 0
+        arg_max = -1
+        for j, label in enumerate(labels):
+            prob = multivariate_normal.pdf(sample, mean=mu, cov=sigma) * priors[j]
+            if prob > max_prob:
+                max_prob = prob
+                arg_max = j
+        print(arg_max)
+        classes[i] = arg_max
+    return classes
 
 
 if __name__ == '__main__':
@@ -22,6 +38,12 @@ if __name__ == '__main__':
     mu = mu_estimate_ml(train_data)
     sigma = sigma_estimate_ml(train_data, mu)
 
-    priors = calc_prior_probabilities(train_labels)
+    labels, priors = calc_prior_probabilities(train_labels)
+
+    test_data = np.genfromtxt('data/Reduced Fashion-MNIST/Test_Data.csv', delimiter=',')
+    test_labels = np.genfromtxt('data/Reduced Fashion-MNIST/Test_Labels.csv', delimiter=',')
+
+    estimated_classes = classify_baysian(test_data, mu, sigma, priors, labels)
+    print(estimated_classes)
 
 
