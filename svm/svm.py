a9593404a10508fe143e6264b9cbba7fc61dbb4c
Sahar Rajabi <sahar.rajabi76@gmail.com>
2019-05-25 14:55:37 +0430

    find svm hyperplane + plot.

--- svm/svm.py
+++ svm/svm.py
@@ -20,7 +20,40 @@ def phi(sample):
 
 
 def apply_kernel(sample, data):
-    return sum([np.matmul(phi(sample), phi(i)) for i in data])
+    return sum([np.matmul(phi(sample), phi(i)) for i in data])/1000000
+
+
+def weight_derivative(weight, data, labels, landa):
+    d_w = np.zeros(len(weight))
+    for i, w in enumerate(weight):
+        d_w[i] = landa * w - sum([label * x[i] for label, x in zip(labels, data)])
+    return d_w
+
+
+def find_hyperplane(data, labels, landa, alpha):
+    weights = np.random.rand(len(data[0]))
+    for _ in range(10000):
+        weights -= alpha * weight_derivative(weights, data, labels, landa)
+    return weights
+
+
+def increase_dim(separated_data, data):
+    transformed = np.zeros((len(data), 3))
+    for i, sample in enumerate(separated_data[0]):
+        transformed[i] = np.array([sample[0], sample[1],
+                                   apply_kernel(sample, separated_data[0])])
+    index = len(separated_data[0])
+    for i, sample in enumerate(separated_data[1]):
+        transformed[i+index] = np.array([sample[0], sample[1],
+                                         apply_kernel(sample, separated_data[1])])
+    return transformed
+
+
+def plot_surface(weights, ax):
+    d = 1400000000
+    xx, yy = np.meshgrid(range(-50, 50), range(-50, 50))
+    z = (d - xx * weights[0] - yy * weights[1]) / weights[2]
+    ax.plot_surface(xx, yy, z, color='green')
 
 
 if __name__ == "__main__":
@@ -47,5 +80,14 @@ if __name__ == "__main__":
     ax.scatter([i[0] for i in separated_data[1]],
                [i[1] for i in separated_data[1]],
                [apply_kernel(sample, separated_data[1]) for sample in separated_data[1]])
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    new_data = increase_dim(separated_data, new_data)
+
+    weights = find_hyperplane(new_data, labels, 0.1, 0.1)
+
+    plot_surface(weights, ax)
 
     plt.show()
